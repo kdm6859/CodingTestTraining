3dbca8b76cf9880b8a52970ff8e0e58ecc8deb9c
kdm6859 <kdm6859@naver.com>
2024-05-22 21:43:54 +0900

    Update 0x05_boj_1874_스택수열.cpp

--- CodingTestTraining/0x05/0x05_boj_1874_스택수열.cpp
+++ CodingTestTraining/0x05/0x05_boj_1874_스택수열.cpp
@@ -1,37 +1,37 @@
-////https://www.acmicpc.net/problem/1874
-//#include <bits/stdc++.h>
-//
-//using namespace std;
-//
-//int main() {
-//	stack<int> nums;
-//	queue<char> res;
-//
-//	int n;
-//	cin >> n;
-//
-//	int i = 1;
-//	while (n--) {
-//		int num;
-//		cin >> num;
-//
-//		while () {
-//			if (nums.empty()) {
-//
-//			}
-//			if (nums.top() > num) {
-//				cout << "NO";
-//				return 0;
-//			}
-//			else if (nums.top() == num) {
-//				nums.pop();
-//				res.push('-');
-//			}
-//			else {
-//				
-//			}
-//		}
-//	}
-//
-//	return 0;
-//}
+//https://www.acmicpc.net/problem/1874
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	stack<int> nums;
+	queue<char> res;
+
+	int n;
+	cin >> n;
+
+	int i = 1;
+	while (n--) {
+		int num;
+		cin >> num;
+
+		while () {
+			if (nums.empty()) {
+
+			}
+			if (nums.top() > num) {
+				cout << "NO";
+				return 0;
+			}
+			else if (nums.top() == num) {
+				nums.pop();
+				res.push('-');
+			}
+			else {
+				
+			}
+		}
+	}
+
+	return 0;
+}
